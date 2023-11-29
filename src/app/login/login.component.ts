dfca5de4d536269c1d89eeabe2c31f6ae14b7e49
Nguyen Minh Tuan <tuana9a@gmail.com>
2023-11-29 21:43:10 +0700

    Fix #1: clear jwt cookie before assign new one

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -24,6 +24,7 @@ export class LoginComponent {
   login() {
     this.publicApi.login(this.username, this.password).subscribe((res) => {
       if (res.success && res.data) {
+        this.cookieUtils.delete("jwt");
         this.cookieUtils.set({ name: "jwt", value: res.data.token });
         this.session.authenticated(res.data);
         this.router.navigate(["/term-ids"]);
